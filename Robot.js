cd4d9948d3823581d7253528e9f325f7e9371b96
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-02 18:05:15 +0500

    Add initial list of chat robots with details

---
+++ Robot.js
@@ -0,0 +1,55 @@
+// 🤖 ZHOBCHAT Robots List
+// آپ یہاں روبوٹس کے نام، جنس، تصویر (dp) اور جملے خود بدل سکتے ہیں۔
+
+export const robots = [
+  {
+    name: "Ali",
+    gender: "Male",
+    dp: "https://i.ibb.co/3B5yP3K/male1.png",
+    messages: [
+      "Salam sab ko! 😊",
+      "Kya haal hain?",
+      "Main bhi yahan chat karne aaya hoon 😄"
+    ]
+  },
+  {
+    name: "Bilal",
+    gender: "Male",
+    dp: "https://i.ibb.co/3mCHjL6/male2.png",
+    messages: [
+      "Assalamualaikum!",
+      "Zhob Chat kafi maze ka hai 😎",
+      "Kaun online hai?"
+    ]
+  },
+  {
+    name: "Meena",
+    gender: "Female",
+    dp: "https://i.ibb.co/q0KrYx9/female1.png",
+    messages: [
+      "Hi everyone 💕",
+      "Mujhe yahan chat karna acha lagta hai!",
+      "Nice to see you all 😍"
+    ]
+  },
+  {
+    name: "Sana",
+    gender: "Female",
+    dp: "https://i.ibb.co/Qk6BYZn/female2.png",
+    messages: [
+      "Salam girls and boys 😊",
+      "Kaun new aaya hai group mein?",
+      "Aaj ka topic kya hai?"
+    ]
+  },
+  {
+    name: "Ahsan",
+    gender: "Male",
+    dp: "https://i.ibb.co/N7tbD5r/male3.png",
+    messages: [
+      "Hey friends 👋",
+      "Main Ahsan hoon, ek friendly bot 😄",
+      "Zhob Chat rocks!"
+    ]
+  }
+];
