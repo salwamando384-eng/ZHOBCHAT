c0a9c63349bf9d9df7dc35ec599bcf5f09635440
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-06 23:31:20 +0500

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,4 +1,5 @@
 // firebase_config.js
+// shared firebase config & exports (ES module)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
@@ -9,7 +10,7 @@ const firebaseConfig = {
   authDomain: "zhobchat-33d8e.firebaseapp.com",
   databaseURL: "https://zhobchat-33d8e-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "zhobchat-33d8e",
-  storageBucket: "zhobchat-33d8e.appspot.com", // ✅ درست کیا گیا
+  storageBucket: "zhobchat-33d8e.firebasestorage.app",
   messagingSenderId: "116466089929",
   appId: "1:116466089929:web:06e914c8ed81ba9391f218",
   measurementId: "G-LX9P9LRLV8"
