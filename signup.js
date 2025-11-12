1de22673c2c70e82978472b8194b2de5d64bb8b6
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-12 10:50:22 +0500

    Update signup.js

--- signup.js
+++ signup.js
@@ -3,16 +3,18 @@ import { createUserWithEmailAndPassword, updateProfile } from "https://www.gstat
 import { ref, set } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
 const signupForm = document.getElementById("signupForm");
-const msg = document.getElementById("msg");
+const msg = document.getElementById("signupMsg");
 
 signupForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const name = document.getElementById("name").value.trim();
-  const email = document.getElementById("email").value.trim();
-  const password = document.getElementById("password").value.trim();
+  const name = document.getElementById("su_name").value.trim();
+  const age = document.getElementById("su_age").value.trim();
+  const gender = document.getElementById("su_gender").value;
+  const email = document.getElementById("su_email").value.trim();
+  const password = document.getElementById("su_password").value.trim();
 
-  if (!name || !email || !password) {
+  if (!name || !email || !password || !age || !gender) {
     msg.textContent = "⚠️ تمام خانے پر کریں۔";
     msg.style.color = "red";
     return;
@@ -25,14 +27,17 @@ signupForm.addEventListener("submit", async (e) => {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
 
-    await updateProfile(user, { displayName: name });
+    await updateProfile(user, { displayName: name, photoURL: "default_dp.png" });
 
     await set(ref(db, "users/" + user.uid), {
       uid: user.uid,
       name,
+      age,
+      gender,
       email,
       photoURL: "default_dp.png",
       status: "online",
+      color: "#000000",
       joinedAt: new Date().toLocaleString()
     });
 
