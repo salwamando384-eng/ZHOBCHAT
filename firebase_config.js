7751b4c87a932f15f13cf91655224e9f7e2bfdd4
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-12 10:48:48 +0500

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   authDomain: "zhobchat-33d8e.firebaseapp.com",
   databaseURL: "https://zhobchat-33d8e-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "zhobchat-33d8e",
-  storageBucket: "zhobchat-33d8e.appspot.com", // ✅ درست کیا گیا
+  storageBucket: "zhobchat-33d8e.appspot.com",
   messagingSenderId: "116466089929",
   appId: "1:116466089929:web:06e914c8ed81ba9391f218",
   measurementId: "G-LX9P9LRLV8"
