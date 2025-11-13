b64694939ba40f066b4cc1ad25e392f68dd9dae4
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-13 16:43:53 +0500

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -5,16 +5,14 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-
 
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
-  authDomain: "zhobchat-33d8e.firebaseapp.com",
-  databaseURL: "https://zhobchat-33d8e-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "zhobchat-33d8e",
-  storageBucket: "zhobchat-33d8e.appspot.com",
-  messagingSenderId: "116466089929",
-  appId: "1:116466089929:web:06e914c8ed81ba9391f218"
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
-
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getDatabase(app);
