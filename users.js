ca0cf0d8d80d8c8b4a0df869c231a9d057b45d29
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-13 08:11:16 +0500

    Update users.js

--- users.js
+++ users.js
@@ -1,24 +1,30 @@
-// users.js
-import { auth, db } from "./firebase_config.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { ref, onValue } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+import { db } from './firebase_config.js';
+import { ref, get } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
-const usersTable = document.getElementById("usersTable");
+const usersDiv = document.getElementById('usersList');
+const backBtn = document.getElementById('backBtn');
 
-onAuthStateChanged(auth, user => {
-  if (!user) { window.location.href = "login.html"; return; }
-  const uRef = ref(db, "users");
-  onValue(uRef, snap => {
-    usersTable.innerHTML = "";
-    snap.forEach(child => {
-      const u = child.val();
-      const div = document.createElement("div");
-      div.className = "userRowFull";
-      div.innerHTML = `<img src="${u.dp||'default_dp.png'}" class="dpSmall"/><div><b>${escapeHtml(u.name)}</b><br/>${escapeHtml(u.age)} | ${escapeHtml(u.gender)} | ${escapeHtml(u.city)}<br/><small>${escapeHtml(u.status)}</small></div>
-        <div><button onclick="(function(){ localStorage.setItem('profileView','${child.key}'); location.href='profile.html' })()">Profile</button></div>`;
-      usersTable.appendChild(div);
-    });
+backBtn.addEventListener('click', () => {
+  location.href = 'chat.html';
+});
+
+get(ref(db, 'users')).then(snapshot => {
+  snapshot.forEach(child => {
+    const u = child.val();
+    const div = document.createElement('div');
+    div.innerHTML = `<b>${u.name}</b> (${u.gender}) - ${u.city} 
+      <button onclick="viewProfile('${child.key}')">Profile</button>
+      <button onclick="startPrivateChat('${child.key}')">Chat</button>`;
+    usersDiv.appendChild(div);
   });
 });
 
-function escapeHtml(s=""){ return (s+"").replaceAll("&","&amp;").replaceAll("<","&lt;").replaceAll(">","&gt;"); }
+window.viewProfile = (uid) => {
+  localStorage.setItem('profileView', uid);
+  location.href = 'profile.html';
+}
+
+window.startPrivateChat = (uid) => {
+  localStorage.setItem('chatWith', uid);
+  location.href = 'private_chat.html';
+}
