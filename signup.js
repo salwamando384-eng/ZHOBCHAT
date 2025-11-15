42b4c1abb262c8ca78a8d63edd764c2e40ac7b45
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-15 14:37:44 +0500

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,19 +1,58 @@
-import { auth, db } from "./firebase_config.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { set, ref } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+import { auth, db, storage } from "./firebase_config.js";
 
-document.getElementById("signupBtn").onclick = () => {
-  let email = email.value;
-  let pass = password.value;
+import {
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
-  createUserWithEmailAndPassword(auth, email, pass)
-    .then(user => {
-      set(ref(db, "users/" + user.user.uid), {
-        dp: "default_dp.png",
-        name: "",
-        city: "",
-        about: ""
-      });
-      window.location = "profile.html";
+import {
+  ref,
+  set
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+
+import {
+  ref as sRef,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-storage.js";
+
+
+// ===============================
+//        SIGNUP BUTTON
+// ===============================
+document.getElementById("signupBtn").onclick = async () => {
+
+  const name = document.getElementById("nameInput").value.trim();
+  const email = document.getElementById("emailInput").value.trim();
+  const pass = document.getElementById("passwordInput").value.trim();
+
+  if (name === "" || email === "" || pass === "") {
+    alert("Please fill all fields!");
+    return;
+  }
+
+  try {
+    // Make Account
+    const userCred = await createUserWithEmailAndPassword(auth, email, pass);
+    const uid = userCred.user.uid;
+
+    // Default DP
+    const defaultDP = "default-dp.png";
+
+    // Save user data in DB
+    await set(ref(db, "users/" + uid), {
+      name: name,
+      email: email,
+      dp: defaultDP,
+      age: "",
+      gender: "",
+      city: "",
+      about: ""
     });
+
+    alert("Signup Successful!");
+    location.href = "chat.html";
+
+  } catch (err) {
+    alert(err.message);
+  }
 };
