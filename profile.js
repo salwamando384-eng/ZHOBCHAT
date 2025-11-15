0f26603b28d03811dca6bdbe0c190c1bfb0a8765
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-15 14:05:05 +0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,69 +1,46 @@
-import { auth, db } from "./firebase_config.js";
-import { ref, set, get, child } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
-import { getStorage, ref as sRef, uploadBytes, getDownloadURL } 
-  from "https://www.gstatic.com/firebasejs/11.0.1/firebase-storage.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { auth, db, storage } from "./firebase_config.js";
+import {
+  ref,
+  set,
+  update,
+  onValue
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
-const backBtn = document.getElementById("backBtn");
-const dpUpload = document.getElementById("dpUpload");
-const profileImage = document.getElementById("profileImage");
-const nameInput = document.getElementById("nameInput");
-const aboutInput = document.getElementById("aboutInput");
-const saveBtn = document.getElementById("saveBtn");
+import {
+  ref as sRef,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-storage.js";
 
-const storage = getStorage();
+const uid = auth.currentUser.uid;
+const userRef = ref(db, "users/" + uid);
 
-let currentUser = null;
-
-// Load current user
-onAuthStateChanged(auth, async (user) => {
-  if (!user) {
-    location.href = "index.html";
-    return;
-  }
-
-  currentUser = user;
-
-  const dbRef = ref(db);
-  const snap = await get(child(dbRef, "users/" + user.uid));
-
-  if (snap.exists()) {
-    const data = snap.val();
-    nameInput.value = data.name || "";
-    aboutInput.value = data.about || "";
-    profileImage.src = data.dp || "default_dp.png";
-  }
+// Load profile
+onValue(userRef, snap => {
+  let data = snap.val();
+  document.getElementById("profilePicPreview").src = data.dp;
 });
 
-// DP Upload
-dpUpload.addEventListener("change", async () => {
-  if (dpUpload.files.length === 0) return;
+// Upload DP
+document.getElementById("profilePicInput").onchange = e => {
+  let file = e.target.files[0];
+  let storageRef = sRef(storage, "dp/" + uid);
 
-  const file = dpUpload.files[0];
-  const storagePath = sRef(storage, "dp/" + currentUser.uid + ".jpg");
-
-  await uploadBytes(storagePath, file);
-  const url = await getDownloadURL(storagePath);
-
-  profileImage.src = url;
-
-  await set(ref(db, "users/" + currentUser.uid + "/dp"), url);
-});
-
-// SAVE PROFILE
-saveBtn.addEventListener("click", async () => {
-  if (!currentUser) return;
-
-  await set(ref(db, "users/" + currentUser.uid), {
-    name: nameInput.value.trim(),
-    about: aboutInput.value.trim(),
-    dp: profileImage.src
+  uploadBytes(storageRef, file).then(() => {
+    getDownloadURL(storageRef).then(url => {
+      update(userRef, { dp: url });
+    });
   });
+};
 
-  alert("Profile Updated!");
-});
+saveProfileBtn.onclick = () => {
+  update(userRef, {
+    name: nameInput.value,
+    age: ageInput.value,
+    gender: genderInput.value,
+    city: cityInput.value,
+    about: aboutInput.value
+  });
+};
 
-// Back Button
-backBtn.addEventListener("click", () => {
-  location.href = "chat.html";
-});
+backBtn.onclick = () => window.location = "chat.html";
