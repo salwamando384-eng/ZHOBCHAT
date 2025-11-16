707fcecf5cc2b1520af053968e2b93b8f113da03
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-16 14:10:17 +0500

    Update login.js

--- login.js
+++ login.js
@@ -3,9 +3,7 @@ import {
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
-const loginBtn = document.getElementById("loginBtn");
-
-loginBtn.onclick = () => {
+document.getElementById("loginBtn").onclick = () => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
