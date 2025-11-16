60470af501ad931c1458c7eca970e03fbbb6bc6e
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-16 15:55:16 +0500

    Update signup.js

--- signup.js
+++ signup.js
@@ -10,20 +10,28 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
 document.getElementById("signupBtn").onclick = () => {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (!email || !password) {
+    alert("Enter email & password");
+    return;
+  }
 
   createUserWithEmailAndPassword(auth, email, password)
-    .then(user => {
+    .then((user) => {
       const uid = user.user.uid;
 
+      // Save basic user data
       set(ref(db, "users/" + uid), {
         name: "New User",
         dp: "default_dp.png"
       });
 
-      alert("Signup successful!");
-      location.href = "profile.html";
+      alert("Signup Successful!");
+      window.location.href = "profile.html";
     })
-    .catch(err => alert(err.message));
+    .catch((err) => {
+      alert(err.message);
+    });
 };
