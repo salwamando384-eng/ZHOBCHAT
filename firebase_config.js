e283d13142195d82a68136536070098669ba228a
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-18 07:51:56 +0500

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,36 +1,25 @@
-// ------------------------------------------------------
-//  FINAL FIREBASE CONFIG FOR ZHOBCHAT (FULLY FIXED)
-// ------------------------------------------------------
-
-// Corrected Firebase Config
-const firebaseConfig = {
-  apiKey: "AIzaSyDiso8BvuRZSWko7kTEsBtu99MKKGD7Myk",
-  authDomain: "zhobchat-33d8e.firebaseapp.com",
-  databaseURL: "https://zhobchat-33d8e-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "zhobchat-33d8e",
-  storageBucket: "zhobchat-33d8e.appspot.com",   // ✔ FIXED (Correct domain)
-  messagingSenderId: "116466089929",
-  appId: "1:116466089929:web:06e914c8ed81ba9391f218",
-  measurementId: "G-LX9P9LRLV8"
-};
-
-// ------------------------------------------------------
-//  IMPORT FIREBASE MODULES (CORRECT FOR GITHUB HOSTING)
-// ------------------------------------------------------
+// Firebase Imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-storage.js";
 
-// ------------------------------------------------------
-//  INITIALIZE FIREBASE
-// ------------------------------------------------------
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyDiso8BvuRZSWko7kTEsBtu99MKKGD7Myk",
+  authDomain: "zhobchat-33d8e.firebaseapp.com",
+  databaseURL: "https://zhobchat-33d8e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "zhobchat-33d8e",
+  storageBucket: "zhobchat-33d8e.appspot.com",
+  messagingSenderId: "116466089929",
+  appId: "1:116466089929:web:06e914c8ed81ba9391f218",
+  measurementId: "G-LX9P9LRLV8"
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 const storage = getStorage(app);
 
-// ------------------------------------------------------
-//  EXPORTS
-// ------------------------------------------------------
 export { auth, db, storage };
