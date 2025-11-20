bf26fcc9868854a369ae7f5e12def8ffbe2ff73a
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-20 19:06:06 +0500

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,25 +1,24 @@
 // firebase_config.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-storage.js";
 
-// ----- 100% Correct Firebase Configuration -----
+// Replace these values ONLY if you have different config in Firebase console
 const firebaseConfig = {
   apiKey: "AIzaSyDiso8BvuRZSWko7kTEsBtu99MKKGD7Myk",
   authDomain: "zhobchat-33d8e.firebaseapp.com",
   databaseURL: "https://zhobchat-33d8e-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "zhobchat-33d8e",
-  storageBucket: "zhobchat-33d8e.appspot.com",   // ← یہ درست کیا گیا ہے
+  storageBucket: "zhobchat-33d8e.appspot.com",
   messagingSenderId: "116466089929",
-  appId: "1:116466089929:web:06e914c8ed81ba9391f218"
+  appId: "1:116466089929:web:06e914c8ed81ba9391f218",
+  measurementId: "G-LX9P9LRLV8"
 };
 
-// ----- Initialize Firebase -----
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 const storage = getStorage(app);
 
-export { auth, db, storage };
+export { app, auth, db, storage };
