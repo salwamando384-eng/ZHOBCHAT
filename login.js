00ac7b13b95ea2a2be90ed6df8858b72b74df515
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-26 19:15:10 +0500

    Update login.js

--- login.js
+++ login.js
@@ -3,18 +3,24 @@ import { auth } from "./firebase_config.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 const loginBtn = document.getElementById("loginBtn");
-const loginMsg = document.getElementById("loginMsg");
+const msg = document.getElementById("msg");
 
 loginBtn.onclick = async () => {
-  loginMsg.textContent = "";
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
-  if (!email || !password) { loginMsg.textContent = "Enter email & password"; return; }
 
+  if (!email || !password) {
+    msg.innerText = "Enter email and password.";
+    return;
+  }
+
+  loginBtn.innerText = "Logging in...";
   try {
     await signInWithEmailAndPassword(auth, email, password);
     window.location.href = "chat.html";
   } catch (err) {
-    loginMsg.textContent = err.message;
+    console.error(err);
+    msg.innerText = err.message;
+    loginBtn.innerText = "Login";
   }
 };
