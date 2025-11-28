4151506fb385c5742de3a5d6efbb0768ff0d3cd9
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-28 09:14:39 +0500

    Update users.js

--- users.js
+++ users.js
@@ -1,29 +1,47 @@
-// users.js
 import { auth, db } from "./firebase_config.js";
-import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { ref, onValue } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
-const usersList = document.getElementById("usersList");
+const btnUsers = document.getElementById("btnUsers");
+const usersPopup = document.getElementById("usersPopup");
+const userList = document.getElementById("userList");
 
-onAuthStateChanged(auth, (user) => {
-  if (!user) { window.location.href = "login.html"; return; }
-  loadUsers();
-});
+const detailPopup = document.getElementById("userDetailsPopup");
+const detailDp = document.getElementById("detailDp");
+const detailName = document.getElementById("detailName");
+const detailAge = document.getElementById("detailAge");
+const detailGender = document.getElementById("detailGender");
+const detailCity = document.getElementById("detailCity");
 
-function loadUsers() {
-  onValue(ref(db, "users"), (snap) => {
-    usersList.innerHTML = "";
-    snap.forEach(child => {
-      const u = child.val();
-      const uid = child.key;
-      const div = document.createElement("div");
-      div.className = "user-card";
-      div.innerHTML = `<img src="${u.dp||'default_dp.png'}"><div><strong>${u.name||'User'}</strong><br>${u.city||''}</div>`;
-      div.onclick = () => {
-        localStorage.setItem("chatUser", uid);
-        window.location.href = "private_chat.html";
-      };
-      usersList.appendChild(div);
+btnUsers.onclick = () => {
+    usersPopup.classList.toggle("hide");
+
+    onValue(ref(db, "users"), (snap) => {
+        userList.innerHTML = "";
+        snap.forEach(u => {
+            let data = u.val();
+            userList.innerHTML += `
+                <div data-uid="${u.key}">
+                    <b>${data.name}</b>
+                </div>
+            `;
+        });
     });
-  });
-}
+};
+
+userList.onclick = (e) => {
+    if (!e.target.dataset.uid) return;
+
+    const uid = e.target.dataset.uid;
+
+    onValue(ref(db, "users/" + uid), (snap) => {
+        let d = snap.val();
+
+        detailDp.src = d.dp;
+        detailName.innerHTML = "Name: " + d.name;
+        detailAge.innerHTML = "Age: " + d.age;
+        detailGender.innerHTML = "Gender: " + d.gender;
+        detailCity.innerHTML = "City: " + d.city;
+
+        detailPopup.classList.remove("hide");
+    });
+};
