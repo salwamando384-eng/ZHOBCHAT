9fbd4ad744b3a08569cd9fc1d8c2bdfbe2b54efc
Aziz Mandokhail <salwamando384@gmail.com>
2025-11-30 18:55:26 +0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,45 +1,18 @@
-import { auth, db, OWNER_UID } from "./firebase_config.js";
-import { set, ref } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+// auth.js
+// Common auth helpers used across pages
 
-const signupBtn = document.getElementById("btnSignup");
-const signupDp = document.getElementById("signupDp");
+import { auth } from "./firebase_config.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
-function fileToBase64(file) {
-    return new Promise((resolve) => {
-        const reader = new FileReader();
-        reader.onload = () => resolve(reader.result);
-        reader.readAsDataURL(file);
+export function requireLogin(redirectTo = "login.html") {
+  return new Promise((resolve) => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        window.location.href = redirectTo;
+        resolve(null);
+      } else {
+        resolve(user);
+      }
     });
+  });
 }
-
-signupBtn.onclick = async () => {
-    try {
-        document.getElementById("authError").innerHTML = "Signing up...";
-
-        let dpBase64 = "";
-        if (signupDp.files[0]) {
-            dpBase64 = await fileToBase64(signupDp.files[0]);
-        }
-
-        const email = signupEmail.value;
-        const pass = signupPass.value;
-
-        const userCred = await createUserWithEmailAndPassword(auth, email, pass);
-        const uid = userCred.user.uid;
-
-        await set(ref(db, "users/" + uid), {
-            name: signupName.value,
-            age: signupAge.value,
-            gender: signupGender.value,
-            city: signupCity.value,
-            dp: dpBase64
-        });
-
-        document.getElementById("authSection").classList.add("hide");
-        document.getElementById("mainChat").classList.remove("hide");
-    }
-    catch (e) {
-        document.getElementById("authError").innerHTML = "Signup error";
-    }
-};
