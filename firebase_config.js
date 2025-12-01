c4010b4560a0bf295a85c68584b154ef6e5e8ef9
Aziz Mandokhail <salwamando384@gmail.com>
2025-12-01 16:02:01 +0500

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,9 +1,9 @@
-// Import the functions you need from the SDKs
+// Import Firebase SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
-// Your web app's Firebase configuration
+// Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyDiso8BvuRZSWko7kTEsBtu99MKKGD7Myk",
   authDomain: "zhobchat-33d8e.firebaseapp.com",
